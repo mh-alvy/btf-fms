4ce544f011a947c89bc9994aec54c430af0704bf
Mahamudul Hasan Alvy <alvymahamudulhasan@gmail.com>
2025-08-31 12:28:59 +0600

    Integrate Firestore for Data Storage

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -4,12 +4,13 @@ import { getFirestore } from 'firebase/firestore';
 import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
-  apiKey: "YOUR_PROJECT_API_KEY",
+  apiKey: "AIzaSyDcGGjtPi3vDgRAcMo57S_sTNn_h-VaB-s",
   authDomain: "btf-fee-management-system.firebaseapp.com",
   projectId: "btf-fee-management-system",
-  storageBucket: "btf-fee-management-system.appspot.com",
+  storageBucket: "btf-fee-management-system.firebasestorage.app",
   messagingSenderId: "54551582618",
-  appId: "1:54551582618:web:abcdef1234567890abcdef"
+  appId: "1:54551582618:web:73ba3524f436fb91204c23",
+  measurementId: "G-T5DCRENX0N"
 };
 
 // Initialize Firebase
